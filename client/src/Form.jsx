1e058ba504e99476762da4a3939a508c77a03ac7
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-05-11 17:09:55 -0700

    Add voice recognition as option for AI prompt

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,22 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import {
   FormLabel,
   Textarea,
   Button,
   FormControl,
   Flex,
+  Heading,
 } from "@chakra-ui/react";
 
 export default function CustomForm({ handleSubmit, isLoading, setIsLoading }) {
   const [value, setValue] = useState("");
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [storedTranscript, setStoredTranscript] = useState("");
+  const [voiceCommand, setVoiceCommand] = useState(false);
 
   const handleInputChange = (e) => {
     let inputValue = e.target.value;
     setValue(inputValue);
   };
 
+  useEffect(() => {
+    if (transcript !== "") {
+      console.log(transcript);
+      setStoredTranscript(transcript);
+    }
+  }, [transcript]);
+
+  useEffect(() => {
+    if (voiceCommand === "start") {
+      SpeechRecognition.startListening();
+      setTimeout(() => {
+        setVoiceCommand("end");
+        SpeechRecognition.stopListening();
+      }, 5000);
+    }
+    if (voiceCommand === "end") {
+      const input = storedTranscript;
+      console.log(input);
+      SpeechRecognition.stopListening();
+      const processInput = input.trim();
+      if (processInput === "") {
+        console.log("nothing detected from mic");
+        resetTranscript();
+        return;
+      }
+      handleSubmit(processInput, resetTranscript);
+    }
+  }, [voiceCommand]);
+
   return (
     <>
+      <Button onClick={() => setVoiceCommand("start")} mb="1rem">
+        Try voice command
+      </Button>
+      <p>Command: {transcript}</p>
+      <Heading mb="1rem" as="h3" size="md">
+        Or
+      </Heading>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -30,7 +73,7 @@ export default function CustomForm({ handleSubmit, isLoading, setIsLoading }) {
         }}
       >
         <FormControl>
-          <FormLabel htmlFor="prompt">Enter prompt:</FormLabel>
+          <FormLabel htmlFor="prompt">Enter prompt manually:</FormLabel>
           <Textarea
             id="prompt"
             value={value}
